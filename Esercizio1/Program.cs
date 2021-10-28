39fe868bd8024a9e91f0fd1b2391ef97cf4ca4bd
Elisa Calza <S4700104@studenti.unige.it>
2021-10-28 15:07:12 +0200

    Remove all error

--- Esercizio1/Program.cs
+++ Esercizio1/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Text.RegularExpressions;		//usata per il match
 
 namespace Esercizio1
 {
     class Person
     {
 		private static long nextSocialSN;
-		private readonly String validName = "[A-Z][a-z]+( [A-Z][a-z]+)*";
-		public sealed String name;
-		public sealed String surname;
-		public sealed long socialSN; // social security number
+		private static readonly String validName = @"[A-Z][a-z]+( [A-Z][a-z]+)*";	//la stringa del match richiede la @ prima della stringa
+		public readonly String name;
+		public readonly String surname;
+		public readonly long socialSN; // social security number
 		private Person spouse; // optional :)
 
 		// private auxiliary static methods for validation and security social number
@@ -16,19 +17,21 @@ namespace Esercizio1
 
 		private static Object requireNonNull(Object o) {
 			if (o == null)
-				throw;
+				throw new ArgumentNullException();
 			return o;
 		}
 
 		private static String requireValidName(String name) {
-			if (!name.matches(Person.validName))
-				throw;
+			Regex reg = new Regex(validName);		//usato per creare il match
+			Match m = reg.Match(name);				//usato per vedere se la stringa appartiene  questo match
+			if (!m.Success)
+				throw new ArgumentNullException();
 			return name;
 		}
 
-		private static long nextSocialSN() {
-			if (Person.nextSocialSN < 0)
-				throw;
+		private static long nextSocial() {
+			if (nextSocialSN < 0)
+				throw new ArgumentNullException();
 			return Person.nextSocialSN++;
 		}
 
@@ -36,7 +39,7 @@ namespace Esercizio1
 
 		public static void join(Person p1, Person p2) {
 			if (p1.spouse != null || p2.spouse != null || p1 == p2)
-				throw;
+				throw new ArgumentNullException();
 			p1.spouse = p2;
 			p2.spouse = p1;
 		}
@@ -44,7 +47,7 @@ namespace Esercizio1
 		public static void divorce(Person p1, Person p2) {
 			Person.requireNonNull(p1);
 			if (p1 != p2.spouse)
-				throw;
+				throw new ArgumentNullException(); ;
 			p1.spouse = null;
 			p2.spouse = null;
 		}
@@ -54,7 +57,7 @@ namespace Esercizio1
 		public Person(String name, String surname) {
 			this.name = Person.requireValidName(name);
 			this.surname = Person.requireValidName(surname);
-			this.socialSN = Person.nextSocialSN();
+			this.socialSN = Person.nextSocial();
 		}
 
 		// instance methods
